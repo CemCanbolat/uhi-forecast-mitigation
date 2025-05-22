d5ab7bffb4e6e23a76bf05e12d842838e210b56b
CemCanbolat <cemcanbolat159@gmail.com>
2025-05-23 02:06:43 +0300

    Backend dockerized and deployed, vercel configured

--- api/main.py
+++ api/main.py
@@ -24,11 +24,11 @@ app = FastAPI(title="UHI Forecast API")
 uvicorn api.main:app --reload --port 8001
 """
 
-origins = os.getenv("CORS_ORIGINS", "")
+origins = os.getenv("CORS_ORIGINS", "").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
